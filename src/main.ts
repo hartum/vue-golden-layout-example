87c93836534628f263e84bcd9937924b14dfe273
Ivan Gascon <ivan.gascon.izarzugaza@es.pwc.com>
2021-01-26 21:07:57 +0100

    First commit

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+//--- GOLDEN LAYOUT ---
+import vgl from "vue-golden-layout";
+import "golden-layout/src/css/goldenlayout-base.css";
+import "golden-layout/src/css/goldenlayout-light-theme.css";
+Vue.use(vgl);
 Vue.config.productionTip = false
 
 new Vue({
